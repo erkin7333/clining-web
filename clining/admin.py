cc3e358427d088f6128cec3bad08850af97ca0db
nurilloh2004 <nurillohpython@gmail.com>
2022-11-11 10:58:11 +0500

    admin py

--- clining/admin.py
+++ clining/admin.py
@@ -1,6 +1,50 @@
 from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(ServiceType)
-admin.site.register(RoomCategory)
-admin.site.register(Double)
+from .models import RoomCategory, ServiceType, Price, Orders
+
+
+
+class RoomCategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        'name', 'price'
+    ]
+    list_display_links = [
+        'name'
+    ]
+    class Meta:
+        model = RoomCategory
+
+admin.site.register(RoomCategory, RoomCategoryAdmin)
+
+
+class ServiceTypeAdmin(admin.ModelAdmin):
+    list_display = [
+        'name'
+    ]
+    list_display_links = [
+        'name'
+    ]
+    class Meta:
+        model = ServiceType
+admin.site.register(ServiceType, ServiceTypeAdmin)
+
+class PriceAdmin(admin.ModelAdmin):
+    list_display = [
+        'services', 'price'
+    ]
+    list_display_links = [
+        'services', 'price'
+    ]
+    class Meta:
+        model = Price
+
+admin.site.register(Price, PriceAdmin)
+
+
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = [
+        'roomname', 'roomprice', 'servicename', 'serviceprice'
+    ]
+    class Meta:
+        model = Orders
+
+admin.site.register(Orders, OrdersAdmin)
