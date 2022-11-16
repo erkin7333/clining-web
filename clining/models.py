ed382d109ff7c40731cf77acf3a236145934e95c
nurilloh2004 <nurillohpython@gmail.com>
2022-11-16 12:36:21 +0500

    all

--- clining/models.py
+++ clining/models.py
@@ -115,56 +115,9 @@ class ContactForm(models.Model):
     class Meta:
         verbose_name = "Kontakt"
 
-# class ServiceType(models.Model):
-#     name = models.CharField(_('name'), max_length=120, blank=True, null=True)
-#     price = models.PositiveIntegerField(_('price'),)
-#     def __str__(self):
-#         return f"{self.name} {str(self.price)}"
-#     class Meta:
-#         verbose_name = "Xizmat kursatish turlari"
 
 
 
-# class RoomCategory(models.Model):
-#     name = models.CharField(_('name'), max_length=100, blank=True, null=True)
-#     price = models.PositiveIntegerField(_('price'), )
-#     def __str__(self):
-#         return self.name
-#     class Meta:
-#         verbose_name = "Xona turlari"
-
-
-# class Orders(models.Model):
-#     roomname = models.CharField(_('roomname'), max_length=100)
-#     roomprice = models.PositiveIntegerField(_('roomprice'), )
-#     servicename = models.CharField(_('servicename'), max_length=100)
-#     serviceprice = models.PositiveIntegerField(_('serviceprice'), )
-#     total = models.PositiveIntegerField(_('total'), )  # 150
-    
-#     def __str__(self):
-#         return f"{str(self.roomname)} {str(self.roomprice)}"
-
-# qani bunda choice hullas sal modellarni xato tuzib qoyibsizda shu serviceni ham orderni ichiga qsez yaxwi bo`lardi`
-#masalan qande bo'ladi 
-#bu code lar github da bor be malol o'zgartirsa bo'laveradi shuni tuzib berolasizmi hoz
-#hozrmasu bugun qilib berolishim mumkun
-
-
-
-# class Order(models.Model):
-#     # ServiceChoices = (
-#     #     ('suv', 'suv'),
-#     #     ('gaz', 'gaz')
-#     # )
-#     roomname = models.CharField(max_length=100, blank=True, null=True)
-#     roomprice = models.PositiveIntegerField()
-#     service = models.ManyToManyField(Service, blank=True)
-#     total = models.PositiveIntegerField(blank=True, null=True)
-#     #masalanda mana
-#     #service = models.CharField(max_length=200, choiches=SerciceChoices)
-    
-#     def __str__(self):
-#         return f"{str(self.roomname)} {str(self.roomprice)}"
 class Service(models.Model):
     name = models.CharField(_('name'), max_length=120, blank=True, null=True)
     price = models.PositiveIntegerField(_('price'),)
